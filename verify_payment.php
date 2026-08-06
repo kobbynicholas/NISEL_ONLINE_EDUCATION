4afed149aff13cb8fd8e2c7f60724069f2c80d5c
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2026-08-06 20:05:36 +0000

    Update verify_payment.php

--- verify_payment.php
+++ verify_payment.php
@@ -5,7 +5,10 @@ use PHPMailer\PHPMailer\Exception;
 
 require __DIR__ . "/config/db.php";
 
-require __DIR__ . "/vendor/autoload.php";
+require __DIR__ . "/PHPMailer/src/Exception.php";
+require __DIR__ . "/PHPMailer/src/PHPMailer.php";
+require __DIR__ . "/PHPMailer/src/SMTP.php";
+
 
 // =============================
 // DATABASE CONNECTION
