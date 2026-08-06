c1e24000540cda8497dc7014fd47c402632580a0
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2026-08-06 22:20:18 +0000

    Add bookings management functionality
    This file handles the display of student bookings and allows assigning a tutor to a booking.

---
+++ admin/bookings.php
@@ -0,0 +1,99 @@
+<?php
+
+require "../config/db.php";
+
+
+if(isset($_POST['assign'])){
+
+
+$id=$_POST['id'];
+
+$tutor=$_POST['tutor'];
+
+
+
+$conn->query("
+
+UPDATE bookings
+
+SET tutor_name='$tutor'
+
+WHERE id='$id'
+
+");
+
+
+}
+
+
+$bookings=$conn->query("SELECT * FROM bookings");
+
+
+?>
+
+
+<h2>Student Bookings</h2>
+
+
+<table border="1" width="100%">
+
+
+<tr>
+
+<th>Student</th>
+
+<th>Subjects</th>
+
+<th>Status</th>
+
+<th>Assign Tutor</th>
+
+</tr>
+
+
+<?php while($b=$bookings->fetch_assoc()){ ?>
+
+
+<tr>
+
+<td><?=$b['student_name']?></td>
+
+<td><?=$b['subjects']?></td>
+
+<td><?=$b['payment_status']?></td>
+
+
+<td>
+
+
+<form method="POST">
+
+
+<input type="hidden" name="id"
+value="<?=$b['id']?>">
+
+
+<input name="tutor"
+placeholder="Tutor name">
+
+
+<button name="assign">
+
+Assign
+
+</button>
+
+
+</form>
+
+
+</td>
+
+
+</tr>
+
+
+<?php } ?>
+
+
+</table>
