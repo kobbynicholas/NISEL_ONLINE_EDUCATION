b1b8b3f3e0edb18c6396c9344b1c1b3fa9abed47
kobbynicholas <159589377+kobbynicholas@users.noreply.github.com>
2026-08-08 18:22:17 +0000

    Update db.php

--- config/config/db.php
+++ config/config/db.php
@@ -1,26 +1,22 @@
 <?php
 
 $host = "localhost";
-
-$username = "root";
-
+$user = "root";
 $password = "";
-
 $database = "nisel_online_education";
-
 
 $conn = new mysqli(
     $host,
-    $username,
+    $user,
     $password,
     $database
 );
 
-
-if($conn->connect_error){
-
-    die("Database connection failed: " . $conn->connect_error);
-
+if ($conn->connect_error) {
+    die(
+        "Database connection failed: "
+        . $conn->connect_error
+    );
 }
 
-?>
+$conn->set_charset("utf8mb4");
